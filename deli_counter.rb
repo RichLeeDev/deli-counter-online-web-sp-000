66a921b1484a9748a785e058568e437775f61a19
Richard Lee <richlee81192@gmail.com>
2019-09-18 19:05:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,7 @@
 # Write your code here.
 katz_deli = []
 def line(array)
-  if line(array).length == 0 
-    puts "The line is currently empty."
-    elsif line(array) >= 1 
-    puts 
+ 
 end
 
 def take_a_number(array, joiner)
